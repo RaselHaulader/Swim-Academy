e4e8a042a84d8c202fdef3326a6f64c6f0be0702
Rasel <hauladerrasel2@gmail.com>
2021-12-01 12:11:14 +0600

    banner img update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,11 +3,11 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className="banner py-5 text-center d-flex align-items-center justify-content-center">
-            <div>
+        <div className="banner py-5  d-flex align-items-center ">
+            <div className="mx-5">
                 <h2 className="mt-5">Swimming</h2>
-                <h1 className=" text-uppercase text-center fw-bolder">a healthy acitivity for our health</h1>
-                <p className="text-center w-75 mx-auto">Swimming is a healthy activity that you can continue for a lifetime. It is a low-impact activity for physical and mental health</p>
+                <h1 className=" text-uppercase  fw-bolder">A healthy acitivity </h1>
+                <p className=" w-75">Swimming is a healthy activity <br/> that you can continue  for a lifetime. It is a <br/> low-impact activity for physical and mental health</p>
                 <button className="btn border text-white my-3 px-5 mb-5">Read More</button>
             </div>
         </div>
